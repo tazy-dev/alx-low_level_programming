061ef7feae39b9617d064be6562bccafe4126117
tazy-dev <tazykaharaba2@gmail.com>
2023-08-10 21:50:36 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * Description:  prints exactly "Programming is like building a multilingual puzzle
- * using puts function
+ * Description:  prints "Programming is like building a multilingual puzzle
+ * exactly using puts function
  * Return: Always 0 (Success)
  */
 
