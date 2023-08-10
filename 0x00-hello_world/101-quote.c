ea757d2e98659e7d25d604ab53bdea23d0b9bf46
tazy-dev <tazykaharaba2@gmail.com>
2023-08-10 23:39:57 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  Prints "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19",followed by a new line, to standard error.
+ * Using Printf and Puts isn't allowed
+ *
+ * Return: Return an error 1 (Fail)
+ */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, 59);
+	return (1);
+}
