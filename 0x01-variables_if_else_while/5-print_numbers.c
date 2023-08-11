88d50363b38894c45a287ef65196d5a87846574d
tazy-dev <tazykaharaba2@gmail.com>
2023-08-11 23:14:13 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  Prints base 10 numbers 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			printf("%d\n", i);
+		printf("%d", i);
+	}
+	return (0);
+}
