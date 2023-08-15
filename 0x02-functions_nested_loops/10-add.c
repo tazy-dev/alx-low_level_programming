ba96945c78798086648a518768bbeff6f6395f5a
tazy-dev <tazykaharaba2@gmail.com>
2023-08-15 23:22:09 +0000

    TASK 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two itnegers.
+ * @n1: First operand to be added.
+ * @n2: Second operand to be added.
+ *
+ * Return: Result of Addition.
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
