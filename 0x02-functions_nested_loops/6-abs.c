fac293fcb9f6d1e44b2d5ca22ea7acbbca6da4ac
tazy-dev <tazykaharaba2@gmail.com>
2023-08-15 22:35:41 +0000

    TASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Calculate the absolute value.
+ * @num: The num to be tested.
+ *
+ * Return: The absolute value of the integer.
+ */
+
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
