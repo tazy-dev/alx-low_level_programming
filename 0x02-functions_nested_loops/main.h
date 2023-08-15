a129888f9f06774cdf422a619529840a57eda35a
tazy-dev <tazykaharaba2@gmail.com>
2023-08-15 14:42:29 +0000

    Function prototype

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/*
+ * File: main.h
+ * Auth: tazy-dev
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char );
+void print_alphabet(void);
