776ddb96d29a5dc8aaf77df7ba0bf46c0aee9c43
tazy-dev <tazykaharaba2@gmail.com>
2023-08-16 01:02:34 +0000

    TASK 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0
+ * @num : The value of the times table to be printed.
+ */
+
+void print_times_table(int num)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < num; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			int result = i * j;
+
+			if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
