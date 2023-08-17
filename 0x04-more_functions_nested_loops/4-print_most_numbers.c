1b76d027440c657cef9181afa3761295ba7ec220
tazy-dev <tazykaharaba2@gmail.com>
2023-08-17 22:48:50 +0000

    TASK 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,10 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; n++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num!= 4)
 			_putchar(num + '0');
 	}
+	_putchar('\n');
 }
