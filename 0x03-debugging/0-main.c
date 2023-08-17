ed6b562402ae52ed8663e92a4f9128bd597b2dcf
tazy-dev <tazykaharaba2@gmail.com>
2023-08-17 21:18:17 +0000

    TASK 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test positive or negative function defined in 
+ *	  main.h
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
