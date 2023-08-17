5454bb2dd5e28247b0228a781d05c4072638e5ca
tazy-dev <tazykaharaba2@gmail.com>
2023-08-17 21:14:01 +0000

    Functions Protptype

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*
+ * File: main.h
+ * Auth: tazy-dev
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
