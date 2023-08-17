b02aa6cf1e87618be0ddfaf395ebc3c1f566f0a5
tazy-dev <tazykaharaba2@gmail.com>
2023-08-17 22:36:19 +0000

    Functions Protptype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,7 +10,9 @@
  *       used in the 0x04-more_functions_nested_loops.
  */
 
+int _putchar(char);
 int _isupper(int );
 int _isdigit(int );
 int mul(int a, int b);
+void print_numbers(void);
 #endif /* MAIN_H */
