3a9044be7a62973309fb61875b0f978928cb7a4b
tazy-dev <tazykaharaba2@gmail.com>
2023-08-18 00:04:03 +0000

    TASK 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = size - 1 -i; j > 0; j--)
+			for (j = size - 1 - i; j > 0; j--)
 				_putchar(' ');
 
 			for (k = 0; k <= i; k++)
