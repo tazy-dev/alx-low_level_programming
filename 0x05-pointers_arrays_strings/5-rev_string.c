10644154667ce4491d721842bd9d531d9953b70b
tazy-dev <tazykaharaba2@gmail.com>
2023-08-22 07:35:36 +0000

    TASK 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - A function that reverses a string.
- * @str: The string to be reversed.
+ * @s: The string to be reversed.
  */
 
 void rev_string(char *s)
@@ -10,10 +10,10 @@ void rev_string(char *s)
 	int len = 0;
 	int i = 0;
 	char tmp;
-	
+
 	while (s[i++])
 		len++;
-	for (i =len - 1; i >= (len / 2); i--)
+	for (i = len - 1; i >= (len / 2); i--)
 	{
 		tmp = s[i];
 		s[i] = s[len - i - 1];
