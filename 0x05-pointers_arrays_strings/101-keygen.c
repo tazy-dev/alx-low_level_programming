57257291a9c7567aa93fbdd0b9ab931da89a8cf1
tazy-dev <tazykaharaba2@gmail.com>
2023-08-22 13:37:34 +0000

    TASK 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,10 +19,10 @@ int main(void)
 
 	for (counter = 0; counter < 64; counter++)
 	{
-		password[counter] = rand() % 97;
+		password[counter] = rand() % 78;
 		checksum += (password[counter] + '0');
 		putchar(password[counter] + '0');
-		if ((2772 - checksum) - '0' < 97)
+		if ((2772 - checksum) - '0' < 78)
 		{
 			lastOne = 2772 - checksum - '0';
 			checksum += lastOne;
