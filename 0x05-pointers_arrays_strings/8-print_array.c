b510e4f4799f0d0bcdf6da31e6c71c04ab089b5c
tazy-dev <tazykaharaba2@gmail.com>
2023-08-22 07:47:01 +0000

    TASK 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * func - Prints n elements of an array of integers, followed by a new line.
+ * @a: An Array of integers.
+ * @n: The number of elements to be printed
+ */
+
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			_putchar(a[i] + '0');
+		else
+			printf("%d, ", s[i]);
+	}
+	_putchar('\n');
+}
