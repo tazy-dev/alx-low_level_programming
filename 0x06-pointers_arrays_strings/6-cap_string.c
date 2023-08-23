bd4b651e5a5addf73c6123a4708ad5aa2af1944b
tazy-dev <tazykaharaba2@gmail.com>
2023-08-23 14:39:49 +0000

    TASK 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char seperators[] = {' ', '\t', '\n', ',', ';', '.', '!',
+			     '?', '(', ')', '{', '}'};
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = char(s[i] - 32);
+		else
+			if (s[i] >= 'a' && s[i] <= 'z')
+				for (j = 0; j < 13; j++)
+					if (s[i - 1] == seperators[j])
+						s[i] = char(s[i] - 32);
+		i++;
+	}
+	return (s);
+}
