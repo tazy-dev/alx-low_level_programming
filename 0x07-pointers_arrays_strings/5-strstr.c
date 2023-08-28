41c871d63c6596fef720f99c3882367e6fbc9a5e
tazy-dev <tazykaharaba2@gmail.com>
2023-08-28 19:42:16 +0000

    TASK 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != 0; j++)
 		{
-			if (haystack[i + j] != needle[j]
+			if (haystack[i + j] != needle[j])
 				break;
 			
 		}
