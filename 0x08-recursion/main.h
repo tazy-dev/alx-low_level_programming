b9d6b6adffbd094a1446dddc989a5ff9b629b7a4
tazy-dev <tazykaharaba2@gmail.com>
2023-08-30 07:23:09 +0000

    Function Prototype

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*
+ * File: main.h
+ * Auth: tazy-dev
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x08-recursion.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(i0x08-recursionnt x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
