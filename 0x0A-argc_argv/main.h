9e1a7c0c9e6406fba66e93120a3ad343e15c1b48
tazy-dev <tazykaharaba2@gmail.com>
2023-09-01 07:57:59 +0000

    Functions Prototypes

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*
+ * File: main.h
+ * Auth: tazy-dev
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x0A-argc_argv.
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
