894d47f198291ad0a2989cdff2f80cd5483d8e8b
tazy-dev <tazykaharaba2@gmail.com>
2023-09-07 21:22:54 +0000

    Functions Prototypes

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,12 +8,14 @@
  * Desc: Header file containing declarations for all functions
  *       used in the 0x0B-malloc_free.
  */
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char
+char **strtow(char *str);
+
 
 #endif
