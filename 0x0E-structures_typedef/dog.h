8041e4e6277008add7eff9fa63444eb85a959ba9
tazy-dev <tazykaharaba2@gmail.com>
2023-09-12 01:30:20 +0000

    TASK 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic dog information
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner name
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+
+#endif
