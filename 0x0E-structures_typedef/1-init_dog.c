74833db665013ade28e3d5af99ce433828428f96
tazy-dev <tazykaharaba2@gmail.com>
2023-09-12 01:48:23 +0000

    TASK 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * init_dog - Intialize a varible of type dog.
+ * @d:A pointer to the uninitialized variable
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner name.
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog)):
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
