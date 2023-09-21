fd5e13e238c10dcf97aa6d96a871de63dd0a2ac2
tazy-dev <tazykaharaba2@gmail.com>
2023-09-21 11:06:42 +0000

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "list.h"
+
+/**
+ * list_len - print the number of elements of list_t list
+ * @h: Input singly Linked List
+ * Return: The number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes_number = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes_number++;
+	}
+	return (nodes_number);
+}
