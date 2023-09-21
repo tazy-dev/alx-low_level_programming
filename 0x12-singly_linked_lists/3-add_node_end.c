784c47ab64d4bee46414b44b8dc422da345c007f
tazy-dev <tazykaharaba2@gmail.com>
2023-09-21 11:37:36 +0000

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,5 +30,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}else
-		add_node_end(&((*head)->next), str);
+		return(add_node_end(&((*head)->next), str));
 }
