5b1df5fa565f86a6092f48805dda9af45ce9a5c0
tazy-dev <tazykaharaba2@gmail.com>
2023-12-01 19:59:11 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,9 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	hash_tbl->size = size;
 	hash_tbl->array = malloc(sizeof(hash_node_t *) * size);
-	if (!hash_tbl->array)
+	if (hash_tbl->array == NULL)
 		return (NULL);
 	for (idx = 0; idx < size; idx++)
-	hash_tbl->array[idx] = NULL;
+		hash_tbl->array[idx] = NULL;
 	return (hash_tbl);
 }
